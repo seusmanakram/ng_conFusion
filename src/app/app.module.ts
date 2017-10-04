c2326afe0109b113f9d5138f00ebf217f252f6fd
Usman Akram <ceusmanakram@gmail.com>
2017-10-04 22:17:18 +0500

    Components part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,14 @@
+export class Dish {
+
+    // Properties associated with DISH will help construct menu 
+
+    name:string; // typescript error checks the data type 
+    image:string;
+    category:string;
+    label:string;
+    price:string;
+    description:string;
+
+
+
+}
