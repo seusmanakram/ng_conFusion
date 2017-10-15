3c8baf1175aa6566e66737ab1ecc02d949417897
Usman Akram <ceusmanakram@gmail.com>
2017-10-15 12:28:05 +0500

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,29 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
 import {DishService} from './services/dish.service';
+
+
+import {AppRoutingModule} from './app-routing/app-routing.module'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +51,8 @@ import {DishService} from './services/dish.service';
     HttpModule,
     FlexLayoutModule,
     MatSelectModule , MatGridListModule , MatProgressSpinnerModule , MatInputModule, MatSlideToggleModule, MatDialogModule,
-    MatToolbarModule, MatCardModule, MatButtonModule , MatSliderModule,MatIconModule, MatMenuModule
+    MatToolbarModule, MatCardModule, MatButtonModule , MatSliderModule,MatIconModule, MatMenuModule,
+    AppRoutingModule
   ],
   // exports: [
   //   MatButtonModule,
